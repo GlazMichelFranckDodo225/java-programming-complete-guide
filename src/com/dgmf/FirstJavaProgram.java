03ca32060748ebd202155f5d963d87a521049511
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-03 08:52:39 +0100

    Project Initialization - Difference between "println()" and "print()" methods

---
+++ src/com/dgmf/FirstJavaProgram.java
@@ -0,0 +1,12 @@
+package com.dgmf;
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world !");
+        System.out.println("A new Statement on a new line");
+        System.out.println("============");
+        System.out.print("Hi, ");
+        System.out.print("ok !");
+        System.out.print("\nBye !");
+    }
+}
