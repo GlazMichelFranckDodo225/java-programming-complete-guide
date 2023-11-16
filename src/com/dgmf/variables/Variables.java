aadef55baa7b17a2c316ea0cceb7b1f2ba072a3f
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-16 13:03:00 +0100

    Variables

---
+++ src/com/dgmf/variables/Variables.java
@@ -0,0 +1,14 @@
+package com.dgmf.variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        int mathMarks = 70;
+
+        System.out.println(mathMarks);
+        System.out.println("I scored " + mathMarks + " in Maths");
+
+        mathMarks = 45;
+
+        System.out.println("I have now scored " + mathMarks + " in Maths");
+    }
+}
